026e870fc708da8abbe5953669b2b8115912dfd6
oneminch <27751688+oneminch@users.noreply.github.com>
2019-11-04 16:45:22 -0600

    Product creator reorganization

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,22 @@
 const path = require('path');
 
 module.exports = {
+	mode: 'development',
    entry: './src/main.js',
 	output: {
 		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist')
+		path: path.resolve('./dist')
 	},
 	module: {
 		rules: [{
-		  test: /\.js$/,
-		  exclude: /node_modules/
+			test: /\.js$/,
+			exclude: /node_modules/,
+			use: {
+				loader: "babel-loader",
+				options: {
+					presets: ["babel-preset-env"]
+				}
+			}
 		}]
 	}
 };
