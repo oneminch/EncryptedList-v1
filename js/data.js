83a63e7938cec149e3ff6687b41f59c104d2e928
oneminch <27751688+oneminch@users.noreply.github.com>
2019-11-04 15:17:18 -0600

    Pre-webpack

---
+++ js/data.js
@@ -0,0 +1,277 @@
+const product_list = [
+   // List of all products
+   {
+      name: "Firefox Send",
+      description: "Share files with end-to-end encryption and a link that automatically expires.",
+      url: "https://send.firefox.com",
+      img_url: "./img/firefox.svg",
+      category: [
+         "All", "File Transfer"
+      ],
+      tags: [
+         "all", "open-source", "free"
+      ]      
+   },
+   {
+      name: "Signal",
+      description: "messaging",
+      url: "https://signal.org",
+      img_url: "./img/signal.png",
+      category: [
+         "All", "Messaging"
+      ],
+      tags: [
+         "all", "open-source", "free"
+      ]      
+   },
+   {
+      name: "EteSync",
+      description: "Share files with end-to-end encryption and a link that automatically expires.",
+      url: "https://etesync.com",
+      img_url: "./img/etesync.png",
+      category: [
+         "All", "File Transfer"
+      ],
+      tags: [
+         "all", "open-source", "free"
+      ]      
+   },
+   {
+      name: "SpiderOak",
+      description: "Share files with end-to-end encryption and a link that automatically expires.",
+      url: "https://send.firefox.com",
+      img_url: "./img/spideroak.svg",
+      category: [
+         "All", "File Transfer"
+      ],
+      tags: [
+         "all", "open-source", "free"
+      ]      
+   },
+   {
+      name: "Skype",
+      description: "Share files with end-to-end encryption and a link that automatically expires.",
+      url: "https://www.skype.com/en/",
+      img_url: "./img/skype.svg",
+      category: [
+         "All", "File Transfer"
+      ],
+      tags: [
+         "all", "open-source", "free"
+      ]      
+   },
+   {
+      name: "Wickr",
+      description: "Share files with end-to-end encryption and a link that automatically expires.",
+      url: "https://wickr.com/",
+      img_url: "./img/wickr.png",
+      category: [
+         "All", "File Transfer"
+      ],
+      tags: [
+         "all", "open-source", "free"
+      ]      
+   },
+   {
+      name: "Wire",
+      description: "Share files with end-to-end encryption and a link that automatically expires.",
+      url: "https://wire.com/en/",
+      img_url: "./img/wire.png",
+      category: [
+         "All", "File Transfer"
+      ],
+      tags: [
+         "all", "open-source", "free"
+      ]      
+   },
+   {
+      name: "Telegram",
+      description: "messaging",
+      url: "https://telegram.org",
+      img_url: "./img/telegram.svg",
+      category: [
+         "All", "Messaging"
+      ],
+      tags: [
+         "all", "free"
+      ]      
+   },
+   {
+      name: "WhatsApp",
+      description: "Share files with end-to-end encryption and a link that automatically expires.",
+      url: "https://www.whatsapp.com/",
+      img_url: "./img/whatsapp.svg",
+      category: [
+         "All", "Messaging"
+      ],
+      tags: [
+         "all", "free"
+      ]      
+   },
+   {
+      name: "Threema",
+      description: "Share files with end-to-end encryption and a link that automatically expires.",
+      url: "https://threema.ch/",
+      img_url: "./img/threema.svg",
+      category: [
+         "All", "Messaging"
+      ],
+      tags: [
+         "all", "open-source", "free"
+      ]      
+   },
+   {
+      name: "CryptPad",
+      description: "Share files with end-to-end encryption and a link that automatically expires.",
+      url: "https://cryptpad.fr",
+      img_url: "./img/cryptpad.svg",
+      category: [
+         "All", "File Transfer"
+      ],
+      tags: [
+         "all", "open-source", "free"
+      ]      
+   },
+   {
+      name: "Protonmail",
+      description: "Send e2ee emails",
+      url: "https://protonmail.com",
+      img_url: "./img/protonmail.svg",
+      category: [
+         "All", "Email"
+      ],
+      tags: [
+         "all", "open-source", "freemium"
+      ]      
+   },
+   {
+      name: "Tutanota",
+      description: "Send e2ee emails",
+      url: "https://Tutanota.com",
+      img_url: "./img/tutanota.svg",
+      category: [
+         "All", "Email"
+      ],
+      tags: [
+         "all", "open-source", "freemium"
+      ]      
+   },
+   {
+      name: "Mailfence",
+      description: "Share files with end-to-end encryption and a link that automatically expires.",
+      url: "https://mailfence.com",
+      img_url: "./img/mailfence.png",
+      category: [
+         "All", "Email"
+      ],
+      tags: [
+         "all", "open-source", "free"
+      ]      
+   },
+   {
+      name: "Mega",
+      description: "Store files with end-to-end encryption.",
+      url: "https://mega.nz",
+      img_url: "./img/mega.svg",
+      category: [
+         "All", "File Transfer", "Storage"
+      ],
+      tags: [
+         "all", "open-source", "freemium"
+      ]      
+   },
+   {
+      name: "Tresorit",
+      description: "Share files with end-to-end encryption and a link that automatically expires.",
+      url: "https://tresorit.com",
+      img_url: "./img/tresorit.svg",
+      category: [
+         "All", "File Transfer", "Storage"
+      ],
+      tags: [
+         "all", "open-source", "freemium"
+      ]      
+   },
+   {
+      name: "pDrive",
+      description: "Share files with end-to-end encryption and a link that automatically expires.",
+      url: "https://pdrive.co/",
+      img_url: "./img/pdrive.png",
+      category: [
+         "All", "File Transfer", "Storage"
+      ],
+      tags: [
+         "all", "open-source", "freemium"
+      ]      
+   },
+   {
+      name: "Recall Photos",
+      description: "Share files with end-to-end encryption and a link that automatically expires.",
+      url: "https://app.recall.photos/",
+      img_url: "./img/recallphotos.svg",
+      category: [
+         "All", "File Transfer", "Storage"
+      ],
+      tags: [
+         "all", "open-source", "freemium"
+      ]      
+   },
+   {
+      name: "Photo Vault",
+      description: "Share files with end-to-end encryption and a link that automatically expires.",
+      url: "https://photovault.online/",
+      img_url: "./img/photovault.png",
+      category: [
+         "All", "File Transfer", "Storage"
+      ],
+      tags: [
+         "all", "open-source", "freemium"
+      ]      
+   },
+   {
+      name: "Encrypt My Photos",
+      description: "Share files with end-to-end encryption and a link that automatically expires.",
+      url: "https://encryptmyphotos.com/",
+      img_url: "./img/encryptmyphotos.png",
+      category: [
+         "All", "File Transfer", "Storage"
+      ],
+      tags: [
+         "all", "open-source", "freemium"
+      ]      
+   },
+   {
+      name: "Firefox Notes",
+      description: "Share files with end-to-end encryption and a link that automatically expires.",
+      url: "https://addons.mozilla.org/en-US/firefox/addon/notes-by-firefox/",
+      img_url: "./img/firefoxnotes.png",
+      category: [
+         "All", "File Transfer", "Storage"
+      ],
+      tags: [
+         "all", "open-source", "freemium"
+      ]      
+   },
+   {
+      name: "Standard Notes",
+      description: "Take encrypted notes.",
+      url: "https://app.standardnotes.org",
+      img_url: "./img/snotes.svg",
+      category: [
+         "All", "Productivity", "Notes"
+      ],
+      tags: [
+         "all", "open-source", "freemium"
+      ]      
+   },
+
+];
+
+// List of available tags
+const tag_list = [
+   "all", "beta", "free", "premium", "freemium", "proprietary", "open-source", "decentralized", "cross-platform" //, "", "", "", "", ""
+];
+// List of available categories
+const catergory_list = [
+   "All", "Email", "Notes", "Storage", "Productivity", "Messaging", "Collaboration"  
+];
---
+++ product_creator/obj_generator.js
@@ -0,0 +1,13 @@
+// To generate an encrypted product 
+// object for list from input
+
+class EncryptedProduct {
+   constructor(n,d,u,i,c,t) {
+      this.name = n;
+      this.desc = d;
+      this.url = u;
+      this.img_url = i;
+      this.category = c;
+      this.tags = t;
+   }
+}
